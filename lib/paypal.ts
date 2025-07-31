55841e1eec8c43cdf2a23c9c0e37e5dc09bb7d23
JoseIgnacioTapia <josignaciot@gmail.com>
2025-07-31 18:04:10 -0300

    Jest Test For Access Token

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -2,7 +2,7 @@ const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
 export const paypal = {};
 
-export async function generateAccessToken() {
+async function generateAccessToken() {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
     'base64'
@@ -25,3 +25,5 @@ export async function generateAccessToken() {
     throw new Error(errorMessage);
   }
 }
+
+export { generateAccessToken };
